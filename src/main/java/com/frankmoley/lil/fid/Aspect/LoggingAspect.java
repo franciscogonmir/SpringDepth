00e4bbdf67bbf0eb3d33af03f82a38913cab3fb5
fjgonmir <francisco.gonzalezmiranda@plexus.es>
2022-04-08 12:03:51 +0200

    implement AOP advice : Around

--- src/main/java/com/frankmoley/lil/fid/Aspect/LoggingAspect.java
+++ src/main/java/com/frankmoley/lil/fid/Aspect/LoggingAspect.java
@@ -1,9 +1,8 @@
 package com.frankmoley.lil.fid.Aspect;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,28 +19,36 @@ public class LoggingAspect {
 
     @Pointcut("@annotation(Loggable)")
     public void executeLogging() {
+        //method point cup because it body empty
     }
 
-   /* @Before("executeLogging()")*/
-    @AfterReturning(value = "executeLogging()",returning = "returnValue")
-    public void logMethodCall(JoinPoint joinPoint,Object returnValue) {
+    /* @Before("executeLogging()")*/
+    /*  @AfterReturning(value = "executeLogging()",returning = "returnValue")*/
+    //public void logMethodCall(JoinPoint joinPoint,Object returnValue) {
+    @Around("executeLogging()")
+    public Object logMethodCall(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object returnValue = joinPoint.proceed();
+        long totalTime = System.currentTimeMillis() - startTime;
         StringBuilder message = new StringBuilder("Method: ");
         message.append(joinPoint.getSignature().getName());
+        message.append(" totalTime: ").append(totalTime).append(" ms ");
         Object[] args = joinPoint.getArgs();
         if (args != null && args.length > 0) {
             message.append("args=[ |");
-            Arrays.asList(args).forEach(arg -> {
-                message.append(arg).append(" | ");
-            });
+            Arrays.asList(args).forEach(arg ->
+                    message.append(arg).append(" | ")
+            );
 
-            if(returnValue instanceof Collection){
+            if (returnValue instanceof Collection) {
                 message.append(", returning: ").append(((Collection) returnValue).size()).append(" instance(s)");
-            }else{
+            } else {
                 message.append(", returning: ").append(returnValue.toString());
             }
             message.append("]");
         }
         LOGGER.info(message.toString());
+        return returnValue;
 
     }
 
